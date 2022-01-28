8752960c3c8516f60446db8f902adf51aa46dd89
Walter Luna <walter.luna@gmail.com>
2022-01-27 21:50:38 -0300

    CRUD ready v1.0.2

--- middlewears/validate.js
+++ middlewears/validate.js
@@ -5,12 +5,10 @@ const validarCampos = ( req, res, next ) => {
     const errors = validationResult(req);
     if ( !errors.isEmpty() ){
         return res.status(400).json(errors);
-    }
+    };
 
     next();
 
 }
 
-module.exports = {
-    validarCampos
-}
+module.exports = { validarCampos };
